37365ee39d9702370443321a1a03dd8e522ba22c
Alpha01 <root@rubyninja.org>
2013-10-12 18:41:20 -0700

    Adding post_install.pl

--- post_install.pl
+++ post_install.pl
@@ -1,15 +1,22 @@
 #!/usr/bin/perl
 use strict;
 
-# running this after kickstart install
-
 my $vm_name = $ARGV[0];
 die "Usage $0 [vm-name]\n" if !$vm_name;
 
 print "Shutting down $vm_name\n";
 system("/usr/bin/VBoxManage controlvm $vm_name poweroff");
 
-sleep 5;
+sleep 3;
 
-print "Removing $vm_name netboot\n";
+print "Removing netboot from $vm_name...\n";
 system("/usr/bin/VBoxManage modifyvm $vm_name --boot1 disk");
+if ($? == 0) {
+    print "Done.\n";
+} else {
+    print "Failed to remove netboot on $vm_name!\n";
+    exit 1;
+}
+
+print "Starting $vm_name ..\n";
+system("/usr/lib/virtualbox/VBoxHeadless -s $vm_name &");
